a7b109aa08a790e255d067b4c6f55f252704007f
innaborysova <innaborysova06@gmail.com>
2025-07-27 22:33:44 +0300

    fix

---
+++ EpamNUnit1/Helpers/Logger.cs
@@ -0,0 +1,48 @@
+﻿using log4net;
+using log4net.Config;
+using System.Reflection;
+
+namespace EpamNUnit1.Helpers;
+
+public class Logger
+{
+    private readonly static Logger _logger = new Logger();
+    private ILog _log = LogManager.GetLogger(typeof(Logger));
+
+    private Logger()
+    {
+        var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly());
+        XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+    }
+
+    public static Logger Instance => _logger;
+
+    public static void LogInfo<T>(string message)
+    {
+        Instance._log.Info($"[{typeof(T).Name}] {message}");
+    }
+
+    public static void LogError<T>(string message, Exception? ex = null)
+    {
+        if (ex == null)
+        {
+            Instance._log.Error($"[{typeof(T).Name}] {message}");
+
+        }
+        else
+        {
+            Instance._log.Error($"[{typeof(T).Name}] {message}", ex);
+
+        }
+    }
+
+    public static void LogWarn<T>(string message)
+    {
+        Instance._log.Warn($"[{typeof(T).Name}] {message}");
+    }
+
+    public static void LogDebug<T>(string message)
+    {
+        Instance._log.Debug($"[{typeof(T).Name}] {message}");
+    }
+}
--- EpamNUnit1/Tests/BaseTests.cs
+++ EpamNUnit1/Tests/BaseTests.cs
@@ -1,30 +1,22 @@
 ﻿using EpamNUnit1.Helpers;
-using log4net;
-using log4net.Config;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using OpenQA.Selenium;
-using System.Reflection;
 
 namespace EpamNUnit1.Tests;
 
 public abstract class BaseTests
 {
     protected IWebDriver _driver;
-    protected static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType);
 
     [OneTimeSetUp]
     public void GlobalSetUp()
     {
-        var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly());
-        XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
-
-        Log.Info("=== TEST RUN STARTED ===");
+        Logger.LogInfo<BaseTests>("=== TEST RUN STARTED ===");
     }
 
     [SetUp]
     public virtual void SetUp()
     {
-        Log.Info("Test Started: " + TestContext.CurrentContext.Test.Name);
+        Logger.LogInfo<BaseTests>("Test Started: " + TestContext.CurrentContext.Test.Name);
 
         ConfigManager configManager = ConfigManager.Instance;
         string browser = configManager.Browser;
@@ -41,16 +33,16 @@ public abstract class BaseTests
     {
         if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
         {
-            Log.Error("Test Failed: " + TestContext.CurrentContext.Test.Name);
-            Log.Error("Error: " + TestContext.CurrentContext.Result.Message);
+            Logger.LogError<BaseTests>("Test Failed: " + TestContext.CurrentContext.Test.Name);
+            Logger.LogError<BaseTests>("Error: " + TestContext.CurrentContext.Result.Message);
 
             string testName = TestContext.CurrentContext.Test.MethodName;
             Utility.TakeScreenshot(_driver, testName);
-            Log.Info("Screenshot taken.");
+            Logger.LogInfo<BaseTests>("Screenshot taken.");
         }
         else
         {
-            Log.Info("Test Passed: " + TestContext.CurrentContext.Test.Name);
+            Logger.LogInfo<BaseTests>("Test Passed: " + TestContext.CurrentContext.Test.Name);
         }
 
         if (_driver != null)
@@ -63,6 +55,6 @@ public abstract class BaseTests
     [OneTimeTearDown]
     public void GlobalTearDown()
     {
-        Log.Info("=== TEST RUN FINISHED ===");
+        Logger.LogInfo<BaseTests>("=== TEST RUN FINISHED ===");
     }
 }

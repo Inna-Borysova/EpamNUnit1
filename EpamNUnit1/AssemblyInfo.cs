6db81ed9002037567e00eb502da536a750ed4c07
innaborysova <innaborysova06@gmail.com>
2025-08-31 18:22:45 +0300

    Revert

--- EpamNUnit1/AssemblyInfo.cs
+++ EpamNUnit1/AssemblyInfo.cs
@@ -1,2 +1,3 @@
 ﻿[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
-[assembly: Parallelizable(ParallelScope.None)]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: LevelOfParallelism(1)]
